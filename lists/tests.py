8352495787c5e5869a1b0becd43d70b19ad079b3
james <james@quizlicious.com>
2020-09-01 08:01:55 -0400

    Use Bootstrap to improve layout

--- lists/tests.py
+++ lists/tests.py
@@ -77,6 +77,7 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
+
 class NewItemTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
