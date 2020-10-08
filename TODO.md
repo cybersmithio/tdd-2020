8d36a152e41b2f1ea48d94ddd86c06e03ae5f1f4
james <james@quizlicious.com>
2020-10-08 08:43:55 -0400

    test_my_lists: precreate sessions, move login checks into base

--- TODO.md
+++ TODO.md
@@ -1,8 +1,5 @@
 # To-do
-* Send emails
-* Generating and recognising unique tokens
-* How to authenticate someone in Django
-* What steps will the user have to go through
+* Clean up wait_for stuff in base.py
 
 # Done
 * Get the server running on port 80 instead of 8080
@@ -15,3 +12,7 @@
 * remove hardcoded URLs from forms in list.html and home.html
 * valid input so there are no blanks
 * Remove duplication of validation logic in views
+* Send emails
+* Generating and recognising unique tokens
+* How to authenticate someone in Django
+* What steps will the user have to go through
--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import WebDriverException
 import time
 
 MAX_WAIT = 10
+TEST_EMAIL = os.environ.get('TEST_EMAIL')
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -42,3 +43,13 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
+    def wait_to_be_logged_in(self, email):
+        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for(lambda: self.browser.find_element_by_name('email'))
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
