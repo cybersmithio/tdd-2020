103ff383af51597402c995b3ac3aa1e6abc3d997
james <james@quizlicious.com>
2020-10-21 22:45:05 -0400

    Added missing database migrations for accounts

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -9,10 +9,10 @@ def _get_manager_dot_py(host):
 def reset_database(username, host):
     manage_dot_py = _get_manager_dot_py(host)
     print(f"reset_database: Connecting to staging server with string {username}@{host}")
-    with settings(host_string=f'{username}@{host}'):
-        print("About to run 'run' command")
+    #with settings(host_string=f'{username}@{host}'):
+    #with settings(user=username):
+    with settings(user=username, host_string=host, key_filename='superlists-staging_key.pem', warn_only=True):
         run(f'{manage_dot_py} flush --noinput')
-        print("Ran the 'run' command")
 
 
 def _get_server_env_vars(host):
@@ -23,7 +23,8 @@ def _get_server_env_vars(host):
 def create_session_on_server(username, host, email):
     manage_dot_py = _get_manager_dot_py(host)
     print(f"create_session_on_server: Connecting to staging server with string {username}@{host}")
-    with settings(host_string=f'{username}@{host}'):
+    #with settings(host_string=f'{username}@{host}'):
+    with settings(user=username, host_string=host, key_filename='superlists-staging_key.pem'):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
             session_key = run(f'{manage_dot_py} create_session {email}')
