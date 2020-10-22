2d0c631d049bff30084188c2fcc9f507f77eafe7
james <james@quizlicious.com>
2020-10-21 22:08:44 -0400

    Adding ability to manage staging server database.

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,4 +1,4 @@
-from fabric.api import run
+from fabric.api import run, env
 from fabric.context_managers import settings, shell_env
 
 
@@ -10,7 +10,9 @@ def reset_database(username, host):
     manage_dot_py = _get_manager_dot_py(host)
     print(f"reset_database: Connecting to staging server with string {username}@{host}")
     with settings(host_string=f'{username}@{host}'):
+        print("About to run 'run' command")
         run(f'{manage_dot_py} flush --noinput')
+        print("Ran the 'run' command")
 
 
 def _get_server_env_vars(host):
